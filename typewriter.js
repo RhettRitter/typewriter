86243d55a5b49923509632351a65bbc5ba1453a1
Rhett Ritter <rhett.ritter@hotmail.com>
2022-04-12 15:24:00 +0000

    typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,8 +1,10 @@
 const sentence = 'hello there from lighthouse labs';
-const i = 500;
+let halt = 0;
 for (const char of sentence) {
-   setTimeout((i) => {
-    process.stdout.write(char);
-   },100);
-};
-  
+  setTimeout(() => {
+    process.stdout.write(char)
+  }, halt)
+  halt += 50;
+}
+
+
